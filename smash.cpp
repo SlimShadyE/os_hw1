0bb5ae178f60e87a9dadef2166d07f7d732a15e5
rami <ramiar2410@gmail.com>
2023-05-16 17:59:44 +0000

    fork problem

--- smash.cpp
+++ smash.cpp
@@ -7,7 +7,7 @@
 #include "signals.h"
 
 int main(int argc, char* argv[]) {
-    if(signal(SIGTSTP , ctrlZHandler)==SIG_ERR) {
+    if(signal( SIGTSTP, ctrlZHandler)==SIG_ERR) {
         perror("smash error: failed to set ctrl-Z handler");
     }
     if(signal(SIGINT , ctrlCHandler)==SIG_ERR) {
